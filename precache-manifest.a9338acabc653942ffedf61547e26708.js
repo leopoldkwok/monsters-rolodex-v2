ba229fc49c59d193df2b11b44cd0dfc53b5ce68c
Leopold Kwok <leopoldkwok@gmail.com>
2019-07-18 22:28:02 +0100

    Updates

---
+++ precache-manifest.a9338acabc653942ffedf61547e26708.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3796631bdde6c07df7e5a54e2d79f193",
+    "url": "/monsters-rolodex-v2/index.html"
+  },
+  {
+    "revision": "5443801ea6ec13b763d3",
+    "url": "/monsters-rolodex-v2/static/css/main.0c0d874a.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex-v2/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "5443801ea6ec13b763d3",
+    "url": "/monsters-rolodex-v2/static/js/main.6b698701.chunk.js"
+  },
+  {
+    "revision": "85c3af2dc502c3bd4207",
+    "url": "/monsters-rolodex-v2/static/js/runtime~main.e009041b.js"
+  }
+]);
